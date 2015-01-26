1fb14d722c324469e028345fffc1708307a14796
Alek Grinberg <agrinb@gmail.com>
2015-01-25 19:24:03 -0500

    Update url.rb

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,18 +2,17 @@ require 'net/http'
 require "uri"
 
 class Url < ActiveRecord::Base
-	belongs_to :user
-	validate :validate_url
-	validates :old_url, presence: true
-	validates :short_url, presence: true
+  belongs_to :user
+  validate :validate_url
+  validates :old_url, presence: true
+  validates :short_url, presence: true
   has_many :visits
 
-	def self.random_code
-		[*('A'..'Z')].sample(8).join
-	end	
+  def self.random_code
+    [*('A'..'Z')].sample(8).join
+  end	
 
-
-	 def validate_url
+  def validate_url
     begin
       uri = URI.parse(old_url)
       resp = uri.kind_of?(URI::HTTP)
@@ -24,6 +23,5 @@ class Url < ActiveRecord::Base
       errors.add(:old_url, "is not a valid url")
     end
   end
-
 end
 
